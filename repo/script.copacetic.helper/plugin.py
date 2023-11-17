6e2bed51b8d9820fddf44db57e114f5ad7dd8bf5
Tribalize <christoddsmith@gmail.com>
2023-11-17 04:00:12 -0500

    1st commit

--- repo/script.copacetic.helper/plugin.py
+++
@@ -1,6 +0,0 @@
-# author: realcopacetic, sualfred
-
-from resources.lib.plugin.main import Main
-
-if __name__ == '__main__':
-    Main()
--- repo/script.copacetic.helper/service.py
+++
@@ -1,6 +0,0 @@
-# author: realcopacetic
-
-from resources.lib.service.monitor import Monitor
-
-if __name__ == "__main__":
-    Monitor()
