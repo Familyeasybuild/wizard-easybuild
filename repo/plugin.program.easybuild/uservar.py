53d51c56ba711e26738a89c3b24fd0e16b6c628e
Tribalize <christoddsmith@gmail.com>
2023-11-17 04:02:10 -0500

    Update uservar.py

--- repo/plugin.program.easybuild/uservar.py
+++ repo/plugin.program.easybuild/uservar.py
@@ -1,10 +1,10 @@
 import xbmcaddon
 
 '''#####-----Build File-----#####'''
-buildfile = 'https://wizardeasybuild.000webhostapp.com/family/builds.json'
+buildfile = 'https://raw.githubusercontent.com/Familyeasybuild/wizard-easybuild/master/builds/builds.json'
 
 '''#####-----Notifications File-----#####'''
-notify_url  = 'https://wizardeasybuild.000webhostapp.com/family/notify.txt'
+notify_url  = 'https://raw.githubusercontent.com/Familyeasybuild/wizard-easybuild/master/builds/notify.txt'
 
 '''#####-----Excludes-----#####'''
 excludes  = ['plugin.video.whatever']
