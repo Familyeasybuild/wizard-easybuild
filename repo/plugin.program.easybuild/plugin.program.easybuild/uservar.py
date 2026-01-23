767cb9bcdfe60eca44e9ad011953d6f03d5dc50b
simon <147948148+Familyeasybuild@users.noreply.github.com>
2026-01-23 10:51:46 +0000

    do

---
+++ repo/plugin.program.easybuild/plugin.program.easybuild/uservar.py
@@ -0,0 +1,14 @@
+'''#####-----Build File-----#####'''
+buildfile = 'https://raw.githubusercontent.com/Familyeasybuild/wizard-easybuild/master/builds/builds.json'
+
+'''#####-----Videos File-----#####'''
+videos_url = 'http://CHANGEME'
+
+'''#####-----Notifications File-----#####'''
+notify_url  = 'https://raw.githubusercontent.com/Familyeasybuild/wizard-easybuild/master/builds/notify.txt'
+
+'''#####-----Changelog Directory-----#####'''
+changelog_dir  = 'http://CHANGEME/'
+
+'''#####-----Excludes-----#####'''
+excludes  = ['plugin.video.whatever']
